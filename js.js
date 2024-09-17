b9770175f8a46db1abb708f69bb9f3b7ecd94580
Olga Kruglova <olha.kruhlova@inetum.com>
2024-09-17 12:01:39 +0200

    #made changes to layout

--- js.js
+++ js.js
@@ -7,51 +7,88 @@ $("#info").on("mouseout", function() {
 })
 
 //preventing the page from refreshing
-$("#sizePicker").on("submit", function(buildGrid) {
- makeGrid();
-  buildGrid.preventDefault();
+$("#size-picker").on("submit", function(buildGrid) {
+    makeGrid();
+    buildGrid.preventDefault();
 });
 
 //make a start grid
 $(function() {
-  makeGrid(5, 5);
+    makeGrid();
 });
-var canvas = $("#pixel_canvas");
-//build a table with a function makeGrid(), using a For Loop;
+
+var canvas = $("#canvas");
+var canvasGrid = $("#canvas-grid");
+
 function makeGrid() {
-  canvas.children().remove(); //empty the grid for each submit
-  var rows = $("#input_height").val();
-   for (var r = 0; r < rows; r++) {
-     canvas.append("<tr></tr>");
-  var columns = $("#input_width").val();
-   for (var c = 0; c < columns; c++) {
-     canvas.children().last().append("<td></td>");
-  };
- };
-}
-// change a color of a cell (when it is clicked on) in a  color which is selected from #colorPicker 
-canvas.on("click", "td", function(){ 
-  var colors = $("#colorPicker").val();
+    canvasGrid.children().remove();
+    var rows = $("#input-height").val();
+
+    for (var r = 0; r < rows; r++) {
+        canvasGrid.append("<tr></tr>");
+        var columns = $("#input-width").val();
+
+        for (var c = 0; c < columns; c++) {
+            canvasGrid.children().last().append("<td></td>");
+        };
+    };
+
+    cleanCanvas();
+
+    if (!rows || !columns) {
+        canvas.append("<div class='alert-message'></div>").text("Please enter a valid number of width and height");
+    };
+};
+
+var isMouseDown = false;
+
+// Listen for mousedown event to set isMouseDown to true
+canvasGrid.on("mousedown", function() {
+    isMouseDown = true;
+});
+
+// Listen for mouseup event to set isMouseDown to false
+canvasGrid.on("mouseup", function() {
+    isMouseDown = false;
+});
+
+// change a color of a cell (when it is clicked on or move over)
+canvasGrid.on("click", "td", function(){ 
+  var colors = $("#color-picker").val();
     $(this).css("background-color", colors);
 });
+
+canvasGrid.on("mouseover", "td", function() {
+    if (isMouseDown) {
+        var colors = $("#color-picker").val();
+        $(this).css("background-color", colors);
+    }
+});
+
 //remove the fill from the cell with double click of a mouse
-canvas.on("dblclick", "td", function () {
+canvasGrid.on("dblclick", "td", function () {
   $(this).css("background-color", "");
 });
-//to fill a grid (all cells)
-$(".colorAll").click(
+
+//to fill all grid
+$(".color-all").click(
 	function(){
-    $("table, td").css("background-color", $("#colorPicker").val())
+    $("table, td").css("background-color", $("#color-picker").val())
 	}
 );
+
+function cleanCanvas(){
+    $("table, td").css("background-color", "")
+}
+
 //clean a canvas  
-$(".clearAll").click(
-	function(){
-		$("table, td").css("background-color", "")
-	}
-);  
+$(".clear-all").click(function() {
+        cleanCanvas()    
+    }
+);
+
 //delete a grid
-$(".delCanvas").click(function(){
+$(".delete-canvas").click(function(){
   $("table, td").children().remove();
 });
 
